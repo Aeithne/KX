573369537220e5ca2b8a34c4377d9f870a8a6da8
Aeithne <40723680+Aeithne@users.noreply.github.com>
2019-07-17 17:38:57 -0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,88 @@
+<?php
+
+    
+        try {
+            $conn = new PDO('mysql:host=localhost;dbname=kx', 'root', '');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+          echo 'ERROR: ' . $e->getMessage();
+        }
+
+	(isset($_POST['usuario'])) ? $usuario = $_POST['usuario'] : $usuario = null;
+        (isset($_POST['senha'])) ? $senha = $_POST['senha'] : $senha = null;
+        (isset($_REQUEST['acao'])) ? $acao = $_REQUEST['acao'] : $acao = NULL;
+        (isset($_POST['firstName'])) ? $firstName = $_POST['firstName'] : $firstName = null;
+        (isset($_POST['lastName'])) ? $lastName = $_POST['lastName'] : $lastName = null;
+        (isset($_POST['email'])) ? $email = $_POST['email'] : $email = null;
+        (isset($_POST['username'])) ? $username = $_POST['username'] : $username = null;
+        (isset($_POST['pass'])) ? $pass = $_POST['pass'] : $pass = null;
+        (isset($_POST['confirmaPassword'])) ? $confirmaPassword = $_POST['confirmaPassword'] : $confirmaPassword = null;
+        (isset($_POST['tipo'])) ? $tipo = $_POST['tipo'] : $tipo = null;
+        (isset($_POST['country'])) ? $country = $_POST['country'] : $country = null;  
+        (isset($_POST['erroCadastro'])) ? $erroCadastro = $_POST['erroCadastro'] : $erroCadastro = null;
+		(isset($_POST['erroLogin'])) ? $erroLogin = $_POST['erroLogin'] : $erroLogin = null;
+		(isset($_POST['mensagem'])) ? $mensagem = $_POST['mensagem'] : $mensagem = null;
+		
+		//echo $acao;
+
+	if($acao == "cadastro"){
+
+		$stmt = $conn->prepare("SELECT * FROM usuario WHERE nomeUsuario = '$username' OR email = '$email' ");
+		$stmt->execute();
+		$resultado = $stmt->fetchObject();
+ 		if($resultado){
+			$acao = "erro";
+			$erro = "Usuário já existe!";
+		} else {
+			$nome = $firstName." ".$lastName;
+			$stmt = $conn->prepare("INSERT INTO usuario (nome, senha, email, nomeUsuario, tipo) VALUES (?, ?, ?, ?, ?)");
+			$stmt->bindParam(1, $nome);
+			$stmt->bindParam(2, $pass);
+			$stmt->bindParam(3, $email);
+			$stmt->bindParam(4, $username);
+			$stmt->bindParam(5, $tipo);
+			$stmt->execute();
+
+			print_r($stmt);
+
+			//if($stmt->columnCount)
+			
+			$acao = "inicio";
+		}
+	}
+
+	if($acao == "login"){
+
+		//$sql = ;  
+		$query = $conn->prepare("SELECT nome, senha, nomeUsuario FROM usuario WHERE nomeUsuario = '$usuario' AND senha = '$pass' ");
+		//$query->bindValue(1, $usuario);
+		//$query->bindValue(1, $senha);
+		$query->execute();
+		//print_r($query);
+
+		$resultado = $query->fetchObject();
+ 		if($resultado){
+			$welcome = $resultado->nome;
+			$acao = "inicio"; 
+		}else{
+			$acao = "erro";
+			$erro = "Usuário não encontrado!";
+		}
+
+	}
+
+	if($acao == "inicio"){
+
+		header('Location: ./welcome.html');
+		$_SESSION['loginUsuario'] = $usuario;
+		$_SESSION['nomeUsuario'] = $resultado->nome;
+		echo "Bem Vindo, $resultado->nome";
+	}
+	
+	if($acao == "erro"){
+
+		echo $erro;
+	}
+
+
+
